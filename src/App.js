6e73a69716188f15f5e95369977499146b19abc4
Kevin O. Cruz <kocruz.24@gmail.com>
2022-09-03 01:58:57 -0400

    [86] React Router Link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import Home from './routes/home/home.components';
-import { Routes, Route, Outlet } from 'react-router-dom';
-
-const Navigation = () => {
-	return (
-		<div>
-			<div>
-				<h1>Nav Bar</h1>
-			</div>
-			<Outlet />
-		</div>
-	);
-};
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
 	return (
