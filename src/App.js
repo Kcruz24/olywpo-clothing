c120440f7f045fbb3026779401a04e17e239bec9
Kevin O. Cruz <kocruz.24@gmail.com>
2022-09-09 18:55:52 -0400

    [112] New Shop Page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
 import Navigation from './routes/navigation/navigation.component';
 import { Routes, Route } from 'react-router-dom';
-
-const Shop = () => {
-	return (
-		<div>
-			<h1>Shopping Page</h1>
-		</div>
-	);
-};
+import Shop from './components/shop/shop.component';
 
 const App = () => {
 	return (
