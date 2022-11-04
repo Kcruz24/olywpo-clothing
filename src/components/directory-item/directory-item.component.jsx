08ec4ad66059bd181da5963ac55343793c1b83c8
Kevin O. Cruz <kocruz.24@gmail.com>
2022-11-04 17:29:44 -0400

    [135] Fixing Clashing Styles

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,17 @@
-import './category-item.styles.scss';
+import './directory-item.styles.scss';
 
-const CategoryItem = (props) => {
+const DirectoryItem = (props) => {
 	const { imageUrl, title } = props.category;
 
 	return (
-		<div className="category-container">
+		<div className="directory-item-container">
 			<div
 				className="background-image"
 				style={{
 					backgroundImage: `url(${imageUrl})`,
 				}}
 			/>
-			<div className="category-body-container">
+			<div className="body">
 				<h2>{title}</h2>
 				<p>Shop Now</p>
 			</div>
@@ -19,4 +19,4 @@ const CategoryItem = (props) => {
 	);
 };
 
-export default CategoryItem;
+export default DirectoryItem;
